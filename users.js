67b64d6d143b5f06e1171c6aa69e20a4b853de72
saiarun42 <109194540+saiarun42@users.noreply.github.com>
2024-03-15 15:43:11 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,19 @@
+const users=[
+    {
+    id:1,
+    name:'arun',
+    place:'vijayanagaram'
+    },
+    {
+        id:2,
+        name:'Kishore',
+        place:'gudiwada'
+        },
+     {
+            id:3,
+            name:'Krishna',
+            place:'ventrapragada'
+    }
+    
+    ];
+    module.exports=users;
